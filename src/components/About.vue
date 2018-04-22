eb033e3afc33b2b398aa667dc67f32fa8896e788
keff6 <kev.fallas@gmail.com>
2018-04-22 14:18:18 -0600

    Add vue-router functionality

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>Hello {{ their_name || "User" }}. This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
+</script>
