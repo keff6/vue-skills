d76f86b28d841af8140297f539eb9620d2a04ed8
keff6 <kev.fallas@gmail.com>
2018-04-22 13:59:13 -0600

    Add animations

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -9,14 +9,26 @@
           v-validate="'min: 5'"
           name="skill"
         >
-        <p class="alert" v-if="errors.has('skill')">
-          {{ errors.first('skill') }}
-        </p>
+        <transition 
+          name="alert-in"
+          enter-active-class="animated flipInX"
+          leave-active-class="animated flipOutX"
+        >
+          <p class="alert" v-if="errors.has('skill')">
+            {{ errors.first('skill') }}
+          </p>
+        </transition>
       </form>
       <ul>
+        <transition-group 
+          name="list"
+          enter-active-class="animated bounceInUp"
+          leave-active-class="animated bounceOutDown"
+        >
         <li v-for="(data, index) in skills" :key="index">
           {{ data.skill }}
         </li>
+        </transition-group>
       </ul>
       <p>These are the skills that you possess.</p>    
     </div>
@@ -49,6 +61,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+
 .holder {
   background: #fff;
 }
@@ -93,5 +107,25 @@ input {
   display: inline-block;
   padding: 5px;
   margin-top: -20px;
+}
+
+.alert-in-enter-active {
+  animation: bounce-in 0.5s;
+}
+
+.alert-in-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
