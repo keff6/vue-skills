0c156f464ff7f8418f32eeea9e31b896e6b45fb2
keff6 <kev.fallas@gmail.com>
2018-04-22 14:24:48 -0600

    Add delete functionality

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -27,6 +27,7 @@
         >
         <li v-for="(data, index) in skills" :key="index">
           {{ data.skill }}
+          <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
         </li>
         </transition-group>
       </ul>
@@ -54,6 +55,9 @@ export default {
           console.log("Not valid");
         }
       });
+    },
+    remove(index) {
+      this.skills.splice(index, 1);
     }
   }
 };
@@ -62,6 +66,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 
 .holder {
   background: #fff;
@@ -127,5 +132,9 @@ input {
   100% {
     transform: scale(1);
   }
+}
+
+i {
+  float: right;
 }
 </style>
